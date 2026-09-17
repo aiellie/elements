d23334e565bf5ac5a28a46d900018213eaddd7f3
aiellie <292223168+aiellie@users.noreply.github.com>
2026-09-17 15:58:27 -0400

    feat(registry): make components installable via shadcn

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { InitSnippet } from "@/app/(home)/components/init-snippet"
 import { Separator } from "@/components/ui/separator"
+import { HelloWorld } from "@/registry/aiellie/blocks/hello-world/hello-world"
 
 export default function Page() {
   return (
@@ -12,6 +13,16 @@ export default function Page() {
       </div>
       <InitSnippet />
       <Separator />
+      <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              A simple hello world component
+            </h2>
+          </div>
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <HelloWorld />
+          </div>
+        </div>
     </div>
   )
 }
